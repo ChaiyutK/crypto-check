8d20cd07456455305a828d804c514595d7ee2cd6
Chaiyut <benzcyber53@gmail.com>
2022-06-17 17:19:20 +0700

    git init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Coin from './components/Coin';
 
 function App() {
+
+  const [coinData,setCoinData] = useState([]);
+  const [query,setQuery] = useState("");
+  const [coinElement,setCoinElement] = useState("");
+
+  useEffect(() => {
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    .then((res) => {
+      setCoinData(res.data);
+      setCoinElement(res.data.map((val,index)=>{
+        return(
+          <Coin key={index} data={val} />
+        );
+      }))
+    })
+  },[])
+
+  const search = (event) =>{
+    if(event.key === "Enter")
+    {
+      setCoinElement("");
+      setCoinElement(coinData.filter((val)=>{
+        if(val.name.includes(query))
+        {
+          return  (<Coin data={val} />);
+        }
+      }))
+      
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className='search' type="text" onKeyPress={search} onChange={(e) => {setQuery(e.target.value)}} />
+      <div className="coinlist">
+        <div className="coin-header">
+          <div className='coin-header-item'><p>Rank</p></div>
+          <div className='coin-header-item'><p>Name</p></div>
+          <div className='coin-header-item'><p>price</p></div>
+          <div className='coin-header-item'><p>24h %</p></div>
+          <div className='coin-header-item'><p>Market Cap</p></div>
+        </div>
+      {coinElement}
+      </div>
     </div>
   );
 }
---
+++ src/components/Coin.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Coin = (props) => {
+    const {data} = props;
+    let dollarUSLocale = Intl.NumberFormat('en-US');
+  return (
+    <div className="coin-container">
+        <div className="coin-rank">
+        <p>{data.market_cap_rank}</p>
+        </div>
+        <div className="coin-name">
+        <img src={data.image} alt="" />
+        <p>{data.name}</p>
+        </div>
+        <div className="coin-price">
+        <p>${dollarUSLocale.format(data.current_price)}</p>
+        </div>
+        <div className="coin-change">
+        <p>{data.price_change_percentage_24h.toFixed(2)}%</p>
+        </div>
+        <div className="coin-marketcap">
+        <p>{dollarUSLocale.format(data.market_cap)}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Coin
